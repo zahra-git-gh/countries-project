c78a2bc3d01459b621aab4528a9ef2c294d93463
zahra-gh <zhrww.zrww@gmail.com>
2024-06-20 17:28:57 +0330

    add .

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -8,7 +8,7 @@ export function NotFoundPage() {
   return (
     <section className="w-full h-screen">
       <div 
-        style={{ backgroundImage: "url(/231.jpg)" }}
+        style={{ backgroundImage: "url(./231.jpg)" }}
         className="w-full h-screen bg-cover bg-center flex justify-center items-end"
       >
        <button onClick={clickHandler} className="bg-yellow-400 text-white w-96 h-24 rounded-3xl font-extrabold text-xl mb-36">Back to home</button>
